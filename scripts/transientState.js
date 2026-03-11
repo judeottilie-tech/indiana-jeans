3a3418980858f2d493679a86fbc738cd31cd5d96
Jude Andersen <jude.ottilie@gmail.com>
2026-03-11 11:16:27 -0500

    submission button

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -1,12 +1,17 @@
 const transientState = {
-    ownsBlueJeans: false,
-    socioLocationId: 0
-}
+  ownsBlueJeans: false,
+  socioLocationId: 0,
+};
 
 export const setOwnsBlueJeans = (chosenOwnership) => {
-    transientState.ownsBlueJeans = chosenOwnership
-}
+  transientState.ownsBlueJeans = chosenOwnership;
+};
 
 export const setSocioLocationId = (chosenLocation) => {
-    transientState.socioLocationId = chosenLocation
-}
+  transientState.socioLocationId = chosenLocation;
+};
+
+export const saveSurveySubmission = async () => {
+  console.log("Saving survey to database...");
+  console.log(transientState);
+};
